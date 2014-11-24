ad04b2cfd866ae97140c520e6f70c2b7a786c171
i6o8 <jim.qi101@gmail.com>
2014-11-23 23:20:03 -0800

    trying include_once("config.php")

--- login.php
+++ login.php
@@ -19,12 +19,7 @@
 	//start session
 	session_start();
 	//mysql connection
-	//include_once("config.php");
-	$db_host = '127.0.0.1:3306';
-	$db_username = 'root';
-	$db_password = '';
-	$db_name = 'CPSC304';
-	$connection = new mysqli($db_host, $db_username, $db_password, $db_name);
+	include_once("config.php");
 
 	// Check that the connection was successful, otherwise exit
     if (mysqli_connect_errno()) {
