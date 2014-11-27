adc757deaa5383a6a52d4d21ee11d948d55ef68b
i6o8 <jim.qi101@gmail.com>
2014-11-26 16:40:16 -0800

    work on checkout

--- checkout.php
+++ checkout.php
@@ -1,3 +1,37 @@
+<?php
+
+	//start session
+	session_start();
+	//mysql connection
+	include_once("config.php");
+
+	//$_SESSION['connection'] = $connection;
+
+	// Check that the connection was successful, otherwise exit
+    if (mysqli_connect_errno()) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
+
+	//detect the user action
+	//case1: confirm purchase
+	//case2: cancel purchase
+
+	//check if the request method is post and evaluate the if statements
+	if($_SERVER["REQUEST_METHOD"] == 'POST') {
+		if (isset($_POST["checkout"]) && $_POST["checkout"] ==  "CHECKOUT") {
+			$CID = $_SESSION['CID'];
+			echo "<script type=\"text/2javascript\">document.location.href=\"login.php\";</script>";
+		}
+		elseif (isset($_POST["cancel"]) && $_POST["cancel"] ==  "CANCEL") {
+
+			//move to shopping cart page
+			echo "<script type=\"text/javascript\">document.location.href=\"shoppingcart.php\";</script>";
+			//}
+		}
+	}
+?>
+
 <html>
 <head>
 <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
@@ -12,5 +46,13 @@
 	
 </head>
 <body>
+	<form id="checkout" name="checkout" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+	<table border=0 cellpadding=0 cellspacing=0>
+        <tr><td>Credit Card Number</td><td><input type="text" size=30 name="new_CC_number"</td></tr>
+        <tr><td>Expiry Date</td><td><input type="text" size=30 name="new_expiry_date"</td></tr>
+        <tr><td></td><td><input type="submit" name="checkout" border=0 value="CONFIRM"></td></tr>
+        <tr><td></td><td><input type="submit" name="cancel" border=0 value="CANCEL"></td></tr>
+    </table>
+</form>
 </body>
 </html>
