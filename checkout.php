da57e1e3f4259f8a6534996c71429d7c3f2e6181
i6o8 <jim.qi101@gmail.com>
2014-11-26 18:41:57 -0800

    checkout implementation

--- checkout.php
+++ checkout.php
@@ -19,17 +19,45 @@
 
 	//check if the request method is post and evaluate the if statements
 	if($_SERVER["REQUEST_METHOD"] == 'POST') {
-		if (isset($_POST["checkout"]) && $_POST["checkout"] ==  "CHECKOUT") {
+		if (isset($_POST["checkout"]) && $_POST["checkout"] ==  "CONFIRM") {
 			$CID = $_SESSION['CID'];
-			echo "<script type=\"text/2javascript\">document.location.href=\"login.php\";</script>";
+			$card = $_POST['new_CC_number'];
+			$expiryDate = $_POST['new_expiry_date'];
+			$receiptID = 10;
+			$date = date("Y-m-d");
+			$order_per_day = 10;
+
+			$sql = "SELECT COUNT(receiptID) FROM orderr";
+			$resulttemp = $connection->query($sql);
+			$result = $resulttemp->num_rows;
+
+			if($result<=$order_per_day) {
+				$expectedDate = $date;
+			}
+			else {
+				$temp = floor($result/$order_per_day);
+				$expectedDate = date("Y-m-d", strtotime($Date. ' + '.$temp.' days'));
+			}
+
+
+			//Prepare the sql query
+			$stmt = $connection->prepare("INSERT INTO orderr (receiptID, date, cid, card, expiryDate, expectedDate, deliveredDate) VALUES (?, ?, ?, ?, ?, ?, null)");
+			
+            //bind values and execute if they're set
+			if(isset($receiptID) && isset($date) && isset($CID) && isset($card) && isset($expiryDate) && isset($expectedDate)) {
+			$stmt->bind_param("isiiss", $receiptID, $date, $CID, $card, $expiryDate, $expectedDate);
+
+			//execute the statement
+			$stmt->execute();
 		}
 		elseif (isset($_POST["cancel"]) && $_POST["cancel"] ==  "CANCEL") {
 
 			//move to shopping cart page
 			echo "<script type=\"text/javascript\">document.location.href=\"shoppingcart.php\";</script>";
-			//}
+			//})
 		}
 	}
+}
 ?>
 
 <html>
@@ -49,7 +77,7 @@
 	<form id="checkout" name="checkout" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 	<table border=0 cellpadding=0 cellspacing=0>
         <tr><td>Credit Card Number</td><td><input type="text" size=30 name="new_CC_number"</td></tr>
-        <tr><td>Expiry Date</td><td><input type="text" size=30 name="new_expiry_date"</td></tr>
+        <tr><td>Expiry Date</td><td><input type="date" size=30 name="new_expiry_date"</td></tr>
         <tr><td></td><td><input type="submit" name="checkout" border=0 value="CONFIRM"></td></tr>
         <tr><td></td><td><input type="submit" name="cancel" border=0 value="CANCEL"></td></tr>
     </table>
