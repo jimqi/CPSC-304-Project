fede9910bd80950b1bbdb07edfd0bdcc00f141e6
i6o8 <jim.qi101@gmail.com>
2014-11-26 20:41:04 -0800

    checkout working

--- checkout.php
+++ checkout.php
@@ -1,86 +1,92 @@
 <?php
 
 	//start session
-	session_start();
+session_start();
 	//mysql connection
-	include_once("config.php");
+include_once("config.php");
 
 	//$_SESSION['connection'] = $connection;
 
 	// Check that the connection was successful, otherwise exit
-    if (mysqli_connect_errno()) {
-        printf("Connect failed: %s\n", mysqli_connect_error());
-        exit();
-    }
+if (mysqli_connect_errno()) {
+	printf("Connect failed: %s\n", mysqli_connect_error());
+	exit();
+}
 
 	//detect the user action
 	//case1: confirm purchase
 	//case2: cancel purchase
 
 	//check if the request method is post and evaluate the if statements
-	if($_SERVER["REQUEST_METHOD"] == 'POST') {
-		if (isset($_POST["checkout"]) && $_POST["checkout"] ==  "CONFIRM") {
-			$CID = $_SESSION['CID'];
-			$card = $_POST['new_CC_number'];
-			$expiryDate = $_POST['new_expiry_date'];
-			$receiptID = 10;
-			$date = date("Y-m-d");
-			$order_per_day = 10;
+if($_SERVER["REQUEST_METHOD"] == 'POST') {
+	if (isset($_POST["checkout"]) && $_POST["checkout"] ==  "CONFIRM") {
+		$CID = $_SESSION['CID'];
+		$card = $_POST['new_CC_number'];
+		$expiryDate = $_POST['new_expiry_date'];
+		$receiptID = 10;
+		$date = date("Y-m-d");
+		$order_per_day = 10;
 
-			$sql = "SELECT COUNT(receiptID) FROM orderr";
-			$resulttemp = $connection->query($sql);
-			$result = $resulttemp->num_rows;
+		$sql = "SELECT receiptID FROM orderr";
+		$resulttemp = $connection->query($sql);
+		$result = $resulttemp->num_rows;
 
-			if($result<=$order_per_day) {
-				$expectedDate = $date;
-			}
-			else {
-				$temp = floor($result/$order_per_day);
-				$expectedDate = date("Y-m-d", strtotime($Date. ' + '.$temp.' days'));
-			}
-
+		if($result<=$order_per_day) {
+			$expectedDate = $date;
+		}
+		else {
+			$temp = floor($result/$order_per_day);
+			$expectedDate = date("Y-m-d", strtotime($Date. ' + '.$temp.' days'));
+		}
 
 			//Prepare the sql query
-			$stmt = $connection->prepare("INSERT INTO orderr (receiptID, date, cid, card, expiryDate, expectedDate, deliveredDate) VALUES (?, ?, ?, ?, ?, ?, null)");
-			
+		$stmt = $connection->prepare("INSERT INTO orderr (receiptID, date, cid, card, expiryDate, expectedDate, deliveredDate) VALUES (null, ?, ?, ?, ?, ?, null);");
+
             //bind values and execute if they're set
-			if(isset($receiptID) && isset($date) && isset($CID) && isset($card) && isset($expiryDate) && isset($expectedDate)) {
-			$stmt->bind_param("isiiss", $receiptID, $date, $CID, $card, $expiryDate, $expectedDate);
+		if(isset($receiptID) && isset($date) && isset($CID) && isset($card) && isset($expiryDate) && isset($expectedDate)) {
+			$stmt->bind_param("siiss", $date, $CID, $card, $expiryDate, $expectedDate);
 
 			//execute the statement
 			$stmt->execute();
-		}
-		elseif (isset($_POST["cancel"]) && $_POST["cancel"] ==  "CANCEL") {
 
-			//move to shopping cart page
-			echo "<script type=\"text/javascript\">document.location.href=\"shoppingcart.php\";</script>";
-			//})
+			if($stmt->error) {
+				printf("<b>Error: %s.</b>\n", $stmt->error);
+			} else {
+				echo "<b>Order Added</b>";
+			}
 		}
 	}
+	elseif (isset($_POST["cancel"]) && $_POST["cancel"] ==  "CANCEL") {
+
+			//move to shopping cart page
+		echo "<script type=\"text/javascript\">document.location.href=\"shoppingcart.php\";</script>";
+			//})
+	}
 }
+
 ?>
 
 <html>
 <head>
-<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-<meta content="utf-8" http-equiv="encoding">
+	<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+	<meta content="utf-8" http-equiv="encoding">
 
-<title> CPSC 304 - Shopping Cart </title>
+	<title> CPSC 304 - Shopping Cart </title>
 
 <!--
 	stylesheet
 -->
-	<link href="styles.css" rel="stylesheet" type="text/css">
-	
+<link href="styles.css" rel="stylesheet" type="text/css">
+
 </head>
 <body>
 	<form id="checkout" name="checkout" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-	<table border=0 cellpadding=0 cellspacing=0>
-        <tr><td>Credit Card Number</td><td><input type="text" size=30 name="new_CC_number"</td></tr>
-        <tr><td>Expiry Date</td><td><input type="date" size=30 name="new_expiry_date"</td></tr>
-        <tr><td></td><td><input type="submit" name="checkout" border=0 value="CONFIRM"></td></tr>
-        <tr><td></td><td><input type="submit" name="cancel" border=0 value="CANCEL"></td></tr>
-    </table>
-</form>
+		<table border=0 cellpadding=0 cellspacing=0>
+			<tr><td>Credit Card Number</td><td><input type="text" size=30 name="new_CC_number"</td></tr>
+			<tr><td>Expiry Date</td><td><input type="date" size=30 name="new_expiry_date"</td></tr>
+			<tr><td></td><td><input type="submit" name="checkout" border=0 value="CONFIRM"></td></tr>
+			<tr><td></td><td><input type="submit" name="cancel" border=0 value="CANCEL"></td></tr>
+		</table>
+	</form>
 </body>
 </html>
--- project3.sql
+++ project3.sql
@@ -3,28 +3,28 @@ SET FOREIGN_KEY_CHECKS=0;
 drop table if exists Item;
  create table Item
 	(upc int not null PRIMARY KEY,
-	title varchar(20),
+	title varchar(30),
 	type char(3) null,
 	category char(12) null,
 	company char(40) null,
 	year int null,
 	price float null,
 	stock int null,
-	-- CHECK (category='rock' OR category='pop' OR category='rap' OR category='country'
-	-- OR category='classical' OR category='new age' OR category='instrumental'),
+	CHECK (category='rock' OR category='pop' OR category='rap' OR category='country'
+	OR category='classical' OR category='new age' OR category='instrumental'),
 	CHECK (type='cd' OR type='dvd'));
 
 drop table if exists LeadSinger; 
 create table LeadSinger
 	(upc int not null,
     FOREIGN KEY (upc) REFERENCES Item(upc),
-	name varchar(20) not null PRIMARY KEY);
+	name varchar(30) not null PRIMARY KEY);
 
 drop table if exists HasSong;
 create table HasSong
 	(upc int not null, 
     FOREIGN KEY (upc) REFERENCES Item(upc),
-	title varchar(25) not null PRIMARY KEY);
+	title varchar(30) not null PRIMARY KEY);
 
 drop table if exists Customer;
 create table Customer
@@ -36,7 +36,7 @@ create table Customer
 
 drop table if exists Orderr;
 create table Orderr
-	(receiptId int not null PRIMARY KEY,
+	(receiptId int not null PRIMARY KEY AUTO_INCREMENT,
 	date date null,
 	cid int null,
     FOREIGN KEY (cid) REFERENCES Customer(cid),
@@ -70,189 +70,186 @@ create table ReturnItem
  
 
 insert into Item
-values('468791534685', 'CeeLos Greatest Hits', 'cd', 'pop', 'Brick Records', '2012', '19.99', '10');
+values('468791534', 'CeeLos Greatest Hits', 'cd', 'pop', 'Brick Records', '2012', '19.99', '10');
 
 insert into Item
-values('981564587122', '1989', 'cd', 'pop', 'Universal Music Group', '2014', '19.99', '15');
+values('981564587', '1989', 'cd', 'pop', 'Universal Music Group', '2014', '19.99', '15');
  
 insert into Item
-values('485465791264', 'Reclassified', 'cd', 'rap', 'Sony Media', '2013', '22.50', '10');
+values('485465791', 'Reclassified', 'cd', 'rap', 'Sony Media', '2013', '22.50', '10');
 
 insert into Item
-values('234511256485', 'The London Sessions', 'cd', 'classical', 'Sony Media', '2013', '18.99', '10');
+values('234511256', 'The London Sessions', 'cd', 'classical', 'Sony Media', '2013', '18.99', '10');
 
 insert into Item
-values('894516725312', 'Live in Dublin', 'cd', 'instrumental', 'Brick Records', '2012', '19.99', '10');
+values('894516725', 'Live in Dublin', 'cd', 'instrumental', 'Brick Records', '2012', '19.99', '10');
 
 insert into Item
-values('871546565123', 'BobMarley', 'cd', 'new age', 'Universal Music Group', '2001', '15.99', '10');
+values('871546565', 'BobMarley', 'cd', 'new age', 'Universal Music Group', '2001', '15.99', '10');
  
 insert into Item
-values('897451462354', 'Slaves to the Grave', 'cd', 'rock', 'Sony Media', '2015', '17.99', '10');
+values('897451462', 'Slaves to the Grave', 'cd', 'rock', 'Sony Media', '2015', '17.99', '10');
 
 insert into Item
-values('796451235124', 'Atonement', 'dvd', 'null', 'NewAge Media', '2005', '15.98', '10');
+values('796451235', 'Atonement', 'dvd', 'null', 'NewAge Media', '2005', '15.98', '10');
 
 insert into Item
-values('751365214893', 'Finding Nemo', 'dvd', 'null', 'Disney', '2004', '9.99', '10');
+values('751365214', 'Finding Nemo', 'dvd', 'null', 'Disney', '2004', '9.99', '10');
 
 insert into Item
-values('152435278659', 'Perks of Being a Wallflower', 'dvd', 'null', 'NewAge Media', '2011', '21.99', '10');
+values('152435278', 'Perks of Being a Wallflower', 'dvd', 'null', 'NewAge Media', '2011', '21.99', '10');
 
 insert into Item
-values('825421367542', 'The Lego Movie', 'dvd', 'null', 'NewAge Media', '2014', '22.99', '10');
+values('825421367', 'The Lego Movie', 'dvd', 'null', 'NewAge Media', '2014', '22.99', '10');
 
 insert into Item
-values('645512356489', 'Aladdin', 'dvd', 'null', 'Disney', '1992', '12.99', '10');
+values('645512356', 'Aladdin', 'dvd', 'null', 'Disney', '1992', '12.99', '10');
 
 insert into Item
-values('486719375432', 'Shrek 2', 'dvd', 'null', 'Sony Media', '2004', '13.99', '10');
+values('486719375', 'Shrek 2', 'dvd', 'null', 'Sony Media', '2004', '13.99', '10');
 
 insert into Item
-values('572635485728', 'Children of Men', 'dvd', 'null', 'NewAge Media', '2012', '19.99', '10');
+values('572635485', 'Children of Men', 'dvd', 'null', 'NewAge Media', '2012', '19.99', '10');
  
 insert into Item
-values('475629387543', 'Unideal Ape', 'cd', 'rap', 'Universal Music Group', '2003', '14.99', '10');
+values('475629387', 'Unideal Ape', 'cd', 'rap', 'Universal Music Group', '2003', '14.99', '10');
  
 insert into Item
-values('485726399384', 'The Dark Knight', 'dvd', 'null', 'Warner Bros.', '2008', '16.99', '10');
+values('485726399', 'The Dark Knight', 'dvd', 'null', 'Warner Bros.', '2008', '16.99', '10');
  
 insert into Item
-values('295734573849', 'Set You Free', 'cd', 'country', 'Universal Music Group', '2013', '13.99', '10');
+values('295734573', 'Set You Free', 'cd', 'country', 'Universal Music Group', '2013', '13.99', '10');
  
 insert into Item
-values('049581783948', 'Mamma Mia!', 'dvd', 'null', 'Warner Bros.', '2009', '14.99', '10');
+values('049581783', 'Mamma Mia!', 'dvd', 'null', 'Warner Bros.', '2009', '14.99', '10');
 
 
 insert into LeadSinger
-values('468791534685', 'Cee Lo Green');
+values('468791534', 'Cee Lo Green');
 
 insert into LeadSinger
-values('981564587122', 'Taylor Swift');
+values('981564587', 'Taylor Swift');
  
 insert into LeadSinger
-values('485465791264', 'Iggy Azaelea');
+values('485465791', 'Iggy Azaelea');
 
 insert into LeadSinger
-values('234511256485', 'Brian Larson');
+values('234511256', 'Brian Larson');
 
 insert into LeadSinger
-values('894516725312', 'Chris Paine');
+values('894516725', 'Chris Paine');
 
 insert into LeadSinger
-values('871546565123', 'Bob Marley');
+values('871546565', 'Bob Marley');
  
 insert into LeadSinger
-values('897451462354', 'Ten Degrees of Boredom');
+values('897451462', 'Ten Degrees of Boredom');
  
 insert into LeadSinger
-values('475629387543', 'The Raw Suckers');
+values('475629387', 'The Raw Suckers');
  
 insert into LeadSinger
-values('295734573849', 'Kevin Hardy');
+values('295734573', 'Kevin Hardy');
 
 
 insert into HasSong
-values('468791534685', 'Forget You');
+values('468791534', 'Forget You');
 
 insert into HasSong
-values('468791534685', 'If Only');
+values('468791534', 'If Only');
 
 insert into HasSong
-values('468791534685', 'Bawse');
+values('468791534', 'Bawse');
 
 insert into HasSong
-values('468791534685', 'Crazy');
+values('468791534', 'Crazy');
 
 insert into HasSong
-values('981564587122', 'Shake It Off');
+values('981564587', 'Shake It Off');
 
 insert into HasSong
-values('981564587122', 'Blank Space');
+values('981564587', 'Blank Space');
 
 insert into HasSong
-values('981564587122', 'New You Were Trouble');
+values('981564587', 'New You Were Trouble');
 
 insert into HasSong
-values('981564587122', 'Basic Girls');
+values('981564587', 'Basic Girls');
 
 insert into HasSong
-values('485465791264', 'Fancy');
+values('485465791', 'Fancy');
 
 insert into HasSong
-values('485465791264', 'Basic Girls');
+values('485465791', 'Three Dumb Things');
 
 insert into HasSong
-values('485465791264', 'Three Dumb Things');
+values('485465791', 'Iggy Eh');
 
 insert into HasSong
-values('485465791264', 'Iggy Eh');
+values('234511256', 'Who Cares');
 
 insert into HasSong
-values('234511256485', 'Who Cares');
+values('234511256', 'House Not Building');
 
 insert into HasSong
-values('234511256485', 'House Not Building');
+values('234511256', 'Crew');
 
 insert into HasSong
-values('234511256485', 'Crew');
+values('234511256', 'Gotcha Back');
 
 insert into HasSong
-values('234511256485', 'Gotcha Back');
+values('894516725', 'What If You And I');
 
 insert into HasSong
-values('894516725312', 'What If You And I');
+values('894516725', 'Honey Baby');
 
 insert into HasSong
-values('894516725312', 'Honey Baby');
+values('894516725', 'Crooning For Ya');
 
 insert into HasSong
-values('894516725312', 'Crooning For Ya');
+values('871546565', 'No Woman, No Cry');
 
 insert into HasSong
-values('871546565123', 'No Woman, No Cry');
+values('871546565', 'Redemption Song');
 
 insert into HasSong
-values('871546565123', 'Redemption Song');
+values('871546565', 'Three Little Birds');
 
 insert into HasSong
-values('871546565123', 'Three Little Birds');
-
-insert into HasSong
-values('871546565123', 'Stir It Up');
+values('871546565', 'Stir It Up');
  
 insert into HasSong
-values('897451462354', 'Hatred of the Devil');
+values('897451462', 'Hatred of the Devil');
 
 insert into HasSong
-values('897451462354', 'Revenge of Kosta');
+values('897451462', 'Revenge of Kosta');
 
 insert into HasSong
-values('897451462354', 'Korra the Brave');
+values('897451462', 'Korra the Brave');
 
 insert into HasSong
-values('897451462354', 'Cracked Glasses');
+values('897451462', 'Cracked Glasses');
 
 insert into HasSong
-values('475629387543', 'Blame the Socialists');
+values('475629387', 'Blame the Socialists');
 
 insert into HasSong
-values('475629387543', 'Yeah');
+values('475629387', 'Yeah');
 
 insert into HasSong
-values('475629387543', 'Sucking');
+values('475629387', 'Sucking');
 
 insert into HasSong
-values('475629387543', 'BAM');
+values('475629387', 'BAM');
  
 insert into HasSong
-values('295734573849', 'Nobody Like You');
+values('295734573', 'Nobody Like You');
 
 insert into HasSong
-values('295734573849', 'Hard Hitting');
+values('295734573', 'Hard Hitting');
 
 insert into HasSong
-values('295734573849', 'Push Shove Pull');
+values('295734573', 'Push Shove Pull');
 
 commit;
 
