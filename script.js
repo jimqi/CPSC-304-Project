44f853de8cfd94fb26f2ba6dd489a2c400b36d95
Cindy Ngan <cindy.ngan@alumni.ubc.ca>
2014-11-26 23:17:50 -0800

    updated nav bar script

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
+"<li><a href='return.php'>Clerk</a></li>" +
+"<li><a href='manager.php'>Manager</a></li>" +
 "<li><a href='shoppingcart.php'>Shopping Cart</a></li>" +
-"<li><a href='add-item.php'>Add Items</a></li>" +
+"<li><a href='add-items.php'>Add Items to Cart</a></li>" +
 "<li><a href='checkout.php'>Checkout</a></li>" +
-"<li><a href='login.php'>Logout</a></li>" +
+"<li><a href='login.php'>Customer Login/Logout</a></li>" +
 "</ul>";
